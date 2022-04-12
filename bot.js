3790d8fea6185dce458fbfd95dc7cfa5a0a8dfe0
Reyderr <wow19973055@gmail.com>
2022-04-12 12:40:51 +0300

    heroku_changes

--- bot.js
+++ bot.js
@@ -1,8 +1,12 @@
-const { Telegraf } = require('telegraf')
-const BOT_TOKEN = "1060761585:AAG2irK2Gxl5qZrozZ8AZyhrTthRLqjWyoU"
-const bot = new Telegraf(BOT_TOKEN) //сюда помещается токен, который дал botFather
-bot.start((ctx) => ctx.reply('Welcome')) //ответ бота на команду /start
+const { Telegraf } = require('telegraf');
+const {searchVideo} = require("./search");
+
+const bot = new Telegraf("1060761585:AAG2irK2Gxl5qZrozZ8AZyhrTthRLqjWyoU")
+
+bot.start((ctx) => ctx.reply('Привіт. Цей бот створений для полегшення пошуку відео по тегу для перегляду за їжею.'));
 bot.help((ctx) => ctx.reply('Send me a sticker')) //ответ бота на команду /help
-bot.on('sticker', (ctx) => ctx.reply('')) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
+bot.on('text', (ctx) => searchVideo(ctx.message.text,7, function(res){ ctx.reply(res);})) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
 bot.hears('hi', (ctx) => ctx.reply('Hey there')) // bot.hears это обработчик конкретного текста, данном случае это - "hi"
 bot.launch() // запуск бота
+
+
---
+++ search.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+const youtubeToken = "AIzaSyDDpQKeop36bE_lZRJOat2M8M31cyLeNWY";
+
+function  createLink(id = '') {
+    var link = '';
+    link = `https://www.youtube.com/watch?v=${id}`;
+    return link;
+}
+
+const searchVideo = (query='', limiter,cb) =>
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${limiter}&q=${query}&key=${youtubeToken}`)
+        .then(res => res.json())
+        .then(json => cb(createLink(json.items[0].id.videoId)))
+        .catch(_ => {
+            throw 'API error while fetching the data.'
+        });
+
+module.exports = {
+    searchVideo
+}
