5662f063a1c4aabe8e2cdfc2b6038d97aaafc493
Reyderr <wow19973055@gmail.com>
2022-05-26 15:54:46 +0300

    add some inline buttons && add search settings

--- bot.js
+++ bot.js
@@ -8,10 +8,145 @@ bot.start((ctx) => {
     pgBase.addUser(ctx.message.from.first_name,ctx.message.from.id);
 });
 
-//bot.help((ctx) => ctx.reply('Send me a sticker')) //ответ бота на команду /help
+bot.action('order', ctx=>{
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, "Change video order", {
+        reply_markup:{
+            inline_keyboard: [
+                [
+                    {text:"Date",callback_data:'ord_date'},
+                    {text:"Rating",callback_data:'ord_rat'},
+                    {text:"Relevance",callback_data:'ord_rel'},
+                    {text:"Title",callback_data:'ord_title'},
+                    {text:"View Count",callback_data:'ord_count'}
+                ],
+                [
+                    {text:"Back to menu",callback_data:'command'}
+                ]
+            ]
+        }
+    })
+    ctx.answerCbQuery();
+})
 
-bot.on('text', (ctx) => searchVideo(ctx.message.text,7, function(res){ ctx.reply(res);}))
+bot.action('duration', ctx=>{
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, "Change video duration", {
+        reply_markup:{
+            inline_keyboard: [
+                [
+                    {text:"Any",callback_data:'dur_any'},
+                    {text:"Long",callback_data:'dur_long'},
+                    {text:"Medium",callback_data:'dur_medium'},
+                    {text:"Short",callback_data:'dur_short'}
+                ],
+                [
+                    {text:"Back to menu",callback_data:'command'}
+                ]
+            ]
+        }
+    })
+    ctx.answerCbQuery();
+})
+
+bot.action('number_v', ctx=>{
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, "Change video duration", {
+        reply_markup:{
+            inline_keyboard: [
+                [
+                    {text:"1",callback_data:'num_1'},
+                    {text:"2",callback_data:'num_2'},
+                    {text:"3",callback_data:'num_3'}
+                ],
+                [
+                    {text:"Back to menu",callback_data:'command'}
+                ]
+            ]
+        }
+    })
+    ctx.answerCbQuery();
+})
+
+bot.action('command', ctx=>{
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, "Change video settings", {
+        reply_markup:{
+            inline_keyboard: [
+                [
+                    {text:"Order",callback_data:'order'},
+                    {text:"Duration",callback_data:'duration'},
+                    {text:"Number of videos",callback_data:'number_v'}
+                ]
+            ]
+        }
+    })
+})
+
+bot.command('command', ctx=>{
+    bot.telegram.sendMessage(ctx.chat.id, "Change video settings", {
+        reply_markup:{
+            inline_keyboard: [
+                [
+                    {text:"Order",callback_data:'order'},
+                    {text:"Duration",callback_data:'duration'},
+                    {text:"Number of videos",callback_data:'number_v'}
+                ]
+            ]
+        }
+    })
+})
+
+// bot.command('help', ctx=>{
+//     bot.telegram.sendMessage(ctx.chat.id, "Bot info", {
+//         reply_markup:{
+//             keyboard: [
+//                 [
+//                 {text:"test 1"},
+//                 {text:"test 2"}
+//                     ]
+//             ],
+//             resize_keyboard: true,
+//             one_time_keyboard:true
+//         }
+//     })
+// })
+
+
+//bot.on('text', (ctx) => searchVideo(ctx.message.text,7, function(res){ ctx.reply(res);}))
 
 bot.startPolling() // запуск бота
 
 
+// bot.command("inline", (ctx) => {
+//     ctx.reply("Hi there!", {
+//         reply_markup: {
+//             inline_keyboard: [
+//                 /* Inline buttons. 2 side-by-side */
+//                 [ { text: "Button 1", callback_data: "btn-1" }, { text: "Button 2", callback_data: "btn-2" } ],
+//
+//                 /* One button */
+//                 [ { text: "Next", callback_data: "next" } ],
+//
+//                 /* Also, we can have URL buttons. */
+//                 [ { text: "Open in browser", url: "telegraf.js.org" } ]
+//             ]
+//         }
+//     });
+// });
+
+// bot.on('callback_query', (ctx) =>{
+//     const command = ctx.update.callback_query.data;
+//     switch (command) {
+//         case 'order':
+//             test('order');
+//             break
+//
+//         case 'duration':
+//             test('duration');
+//             break
+//     }
+//
+//     ctx.answerCbQuery();
+//
+// });
--- search.js
+++ search.js
@@ -2,40 +2,19 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 const youtubeToken = process.env.YOUTUBE_TOKEN;
 
-/* Order value
-    date – Resources are sorted in reverse chronological order based on the date they were created.
-    rating – Resources are sorted from highest to lowest rating.
-    relevance – Resources are sorted based on their relevance to the search query. This is the default value for this parameter.
-    title – Resources are sorted alphabetically by title.
-    videoCount – Channels are sorted in descending order of their number of uploaded videos.
-    viewCount – Resources are sorted from highest to lowest number of views. For live broadcasts, videos are sorted by number of concurrent viewers while the broadcasts are ongoing.
-*/
-const order = 'rating';
-
-
-/*  VideoDuration value
-    any – Do not filter video search results based on their duration. This is the default value.
-    long – Only include videos longer than 20 minutes.
-    medium – Only include videos that are between four and 20 minutes long (inclusive).
-    short – Only include videos that are less than four minutes long.
-*/
-
-const videoDuration = 'medium';
-
-
 function  createLinks(id) {
     var link = 'https://www.youtube.com/watch?v=';
     return  link + id.videoId;
 }
 
-const searchVideo = (query='', limiter,cb) =>
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoDuration=${videoDuration}&type=video&maxResults=${limiter}&q=${query}&key=${youtubeToken}`)
+const searchVideo = (query='', order = 'rating', videoDuration = 'medium', limiter = 1, cb) => {
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&order=${order}&videoDuration=${videoDuration}&type=video&maxResults=${limiter}&q=${query}&key=${youtubeToken}`)
         .then(res => res.json())
         .then(json => cb(createLinks(json.items[0].id)))
         .catch(_ => {
             throw 'API error while fetching the data.'
         });
-
+}
 module.exports = {
     searchVideo
 }
